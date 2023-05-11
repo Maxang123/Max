2d575bf9f02117c17f690a7650f57044d7f49341
Maxang123 <maxvivit@gmail.com>
2023-05-11 18:57:05 +0700

    my first commit

---
+++ day1(Lvl2.py
@@ -0,0 +1,11 @@
+import math
+from math import squrt,floor,ceil,pow
+from math import * #ไม่เเนะนำ
+import math as m
+
+import time
+for i in range(1000):
+    
+    result += i**2
+print(result)
+import os
